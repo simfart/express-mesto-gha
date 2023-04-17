a895562d9181d30eeab7e0253853d06233af7ad4
Artjugina <simf.i@ya.ru>
2023-04-17 16:49:43 +0300

    добавить пользователя

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const express = require('express')
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
 
 const app = express()
 
-const port = 3000
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-    console.log('Server start')
-})
+mongoose.connect('mongodb://127.0.0.1/mestodb')
 
-app.get('/', (req, res) => {
-    res.send('Hello word')
-})
+app.use('/users', require('./routes/users'));
+
+app.use((req, res, next) => {
+    req.user = {
+        _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    };
+
+    next();
+});
+
+module.exports.createCard = (req, res) => {
+    console.log(req.user._id); // _id станет доступен
+};
+
+app.listen(PORT, () => {
+    console.log('Ссылка на сервер');
+});
---
+++ models/card.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30,
+    },
+    link: {
+        type: String,
+        required: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    likes: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'user',
+        default: undefined
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+
+
+});
+
+module.exports = mongoose.model('user', userSchema); 
