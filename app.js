0426442dcf4122b4ccfc79516fd69f0523cc9536
Artjugina <simf.i@ya.ru>
2023-05-13 17:15:58 +0300

    cors

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT, MONGO_ADRESS } = require('./utils/config');
 
 const app = express();
+
+app.use(cors({
+  origin: [
+    'http://127.0.0.1:27017',
+    'http://simfart.nomoredomains.monster',
+    'https://simfart.nomoredomains.monster',
+  ],
+  method: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
+}));
+
 app.use(requestLogger); // подключаем логгер запросов
 app.use(express.json());
 app.use(cookieParser());
